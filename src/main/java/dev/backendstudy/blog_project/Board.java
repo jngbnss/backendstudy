1b819b2ccb2c662d6642dd502aa2f01bbf3f6e7a
jngbn <jngbnss@naver.com>
2026-04-22 15:36:00 +0900

    feat: 게시판 기본 기능을 위한 Board 엔티티 구현

--- src/main/java/dev/backendstudy/blog_project/Board.java
+++ src/main/java/dev/backendstudy/blog_project/Board.java
@@ -1,22 +1,30 @@
 package dev.backendstudy.blog_project;
-import jakarta.persistence.*;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@jakarta.persistence.Entity
+@Entity
 @Getter
 @NoArgsConstructor // 파라미터가 없는 기본 자동 생성자 자동생성
-@jakarta.persistence.EntityListeners(AuditingEntityListener.class) // 생성/ 수정 시간 자동 기록을 위해 필요
-public class Entity {
+@EntityListeners(AuditingEntityListener.class) // 생성/ 수정 시간 자동 기록을 위해 필요
+public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // DB의 auto increment 사용
     private Long id;
 
-    @Column(nullable = false,length = 100) // 제목은 필수 , 길이는 100자 제한
+    @Column(nullable = false, length = 100) // 제목은 필수 , 길이는 100자 제한
     private String title;
     @Column(columnDefinition = "TEXT", nullable = false)// 내용은 길 수 있으므로
     private String content;
@@ -30,14 +38,14 @@ public class Entity {
 
     //게시글 생성을 위한 생성자
 
-    public Entity(String title, String content, String writer) {
+    public Board(String title, String content, String writer) {
         this.title = title;
         this.content = content;
         this.writer = writer;
     }
 
     //게시글 수정을 위한 메서드
-    public void update(String title,String content){
+    public void update(String title, String content) {
         this.title = title;
         this.content = content;
         //setter 대신 비즈니스 메서드 사용권장
