dd56e28c77d3c1f752eb03554f6bb334fb7a771b
jngbn <jngbnss@naver.com>
2026-04-22 15:35:54 +0900

    docs: 프로젝트 개요 및 기술 스택 추가 (README)

---
+++ src/main/java/dev/backendstudy/blog_project/Board.java
@@ -0,0 +1,45 @@
+package dev.backendstudy.blog_project;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@jakarta.persistence.Entity
+@Getter
+@NoArgsConstructor // 파라미터가 없는 기본 자동 생성자 자동생성
+@jakarta.persistence.EntityListeners(AuditingEntityListener.class) // 생성/ 수정 시간 자동 기록을 위해 필요
+public class Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB의 auto increment 사용
+    private Long id;
+
+    @Column(nullable = false,length = 100) // 제목은 필수 , 길이는 100자 제한
+    private String title;
+    @Column(columnDefinition = "TEXT", nullable = false)// 내용은 길 수 있으므로
+    private String content;
+    @Column(nullable = false)
+    private String writer;
+    @CreatedDate
+    @Column(updatable = false) // 생성 시간은 수정되지 않도록 설정
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    //게시글 생성을 위한 생성자
+
+    public Entity(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+    //게시글 수정을 위한 메서드
+    public void update(String title,String content){
+        this.title = title;
+        this.content = content;
+        //setter 대신 비즈니스 메서드 사용권장
+    }
+}
