f8477001cdc829fe4dfe416df8ef48422422ceb0
jngbnss <jngbnmn@naver.com>
2026-08-07 10:23:59 +0900

    refactor(post): 타임스탬프 설정을 JPA 콜백에서 생성자로 이동

--- src/main/java/dev/backendstudy/ramgstein/post/Post.java
+++ src/main/java/dev/backendstudy/ramgstein/post/Post.java
@@ -10,17 +10,13 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "posts")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED) //이건 뭘까
-//jpa는 파라미터  없는 생성자를 반드시 요구
-//public으로 열면  아무 데서나 가능,
-//protected로 막으면 같은  패키지  or 상속받은 클래스만 호출
-//다른  패키지의 service/controller에서 실수로 못 씀
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false,length =100) // 20으로 고치면 안되나?
+    @Column(nullable = false,length =100)
     private String title;
 
     @Column(nullable = false,columnDefinition = "TEXT")
@@ -39,19 +35,13 @@ public class Post {
         this.title = title;
         this.content = content;
         this.writer = writer;
-    }//이거는  왜 필요한거지?
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
 
     public void update(String title,String content){
         this.title = title;
         this.content = content;
-    }
-    @PrePersist
-    void onCreate(){
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = this.createdAt;
-    }
-    @PreUpdate
-    void onUpdated(){
         this.updatedAt = LocalDateTime.now();
     }
 }
